456b21a34de19a6dadb605d0ed641e90ad3e7a7f
khawla lamsiyeh <61538329+khawlalam@users.noreply.github.com>
2025-06-22 20:01:01 +0000

    add interface

--- src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
+++ src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
@@ -1,7 +1,15 @@
 package ma.enset.ebankingbackend;
 
+import ma.enset.ebankingbackend.entities.Customer;
+import ma.enset.ebankingbackend.repositories.AccountOperationRepository;
+import ma.enset.ebankingbackend.repositories.BankAccountRepository;
+import ma.enset.ebankingbackend.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class EbankingBackendApplication {
@@ -9,5 +17,17 @@ public class EbankingBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(EbankingBackendApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository,
+                            BankAccountRepository bankAccountRepository,
+                            AccountOperationRepository accountOperationRepository) {
+        return args -> {
+            Stream.of("Khawla","Ihssane","Soumia").forEach(name -> {
+                Customer customer = new Customer();
+                customer.setName(name);
+                customer.setEmail(name + "@gmail.com");
+                customerRepository.save(customer);
+            });
+        };
+    }
 }
